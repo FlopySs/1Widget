62f4fab60cb2dcd1ae2adbfab0d80937a4ddb60f
FlopySs <54317752+FlopySs@users.noreply.github.com>
2019-09-13 21:39:34 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:floppy_ss/category.dart';
+
+void main() {
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          // TODO: Determine what properties you'll need to pass into the widget
+          child: Category(),
+        ),
+      ),
+    );
+  }
+}
